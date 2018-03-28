fa60819191b6a039815745bdfadcbf3a56276759
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2018-03-28 19:42:02 +0900

    :sparkles:

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+)
+
+type User struct {
+	ID string `json:"id"`
+	jwt.StandardClaims
+}
+
+func claimAndParse() error {
+	t := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		&User {
+			ID: "hoge",
+		},
+	)
+	sig := []byte("hogehoge")
+	ts, err := t.SignedString(sig)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(ts)
+	tt, err := jwt.Parse(ts, func(t *jwt.Token) (interface{}, error) {
+		return sig, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(tt)
+
+	_, err = jwt.Parse(ts, func(t *jwt.Token) (interface{}, error) {
+		return []byte("aaa"), nil
+	})
+	return err
+}
 
 func main() {
-	fmt.Printf("hoge")
+	e := claimAndParse()
+	fmt.Printf("%+v\n", e)
 }
