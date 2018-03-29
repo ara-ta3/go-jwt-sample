f76d517b37558cc27d69afc7f21651861dccb644
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2018-03-29 14:42:21 +0900

    :sparkles: Impl run with key pair

--- Makefile
+++ Makefile
@@ -1,5 +1,15 @@
 install:
 	go get -u github.com/dgrijalva/jwt-go
 
-run:
+run: app.rsa
 	go run main.go
+
+keypair/openssl:
+	openssl genrsa 4096 > app.rsa
+	openssl rsa -pubout < app.rsa > app.rsa.pub
+
+app.rsa:
+	$(MAKE) keypair/openssl
+
+clean:
+	rm -f app.rsa app.rsa.pub
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"io/ioutil"
 )
 
 type User struct {
@@ -10,37 +11,90 @@ type User struct {
 	jwt.StandardClaims
 }
 
-func claimAndParse() error {
+var (
+	SigKey          = []byte("hogehoge")
+	PublicKeyPath 	= "./app.rsa.pub"
+	PrivateKeyPath 	= "./app.rsa"
+)
+
+func claimAndParseWithSameKey() error {
 	t := jwt.NewWithClaims(
 		jwt.SigningMethodHS256,
 		&User {
 			ID: "hoge",
 		},
 	)
-	sig := []byte("hogehoge")
-	ts, err := t.SignedString(sig)
+	ts, err := t.SignedString(SigKey)
 	if err != nil {
 		return err
 	}
 
 	fmt.Println(ts)
 	tt, err := jwt.Parse(ts, func(t *jwt.Token) (interface{}, error) {
-		return sig, nil
+		return SigKey, nil
 	})
 
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(tt)
+	fmt.Println("parsed")
+	fmt.Printf("%+v\n", tt.Claims)
 
 	_, err = jwt.Parse(ts, func(t *jwt.Token) (interface{}, error) {
 		return []byte("aaa"), nil
 	})
-	return err
+	fmt.Printf("%+v\n", err)
+	return nil
+}
+
+func claimAndParseWithKeyPair() error {
+	privateKey, err := ioutil.ReadFile(PrivateKeyPath)
+	if err != nil {
+		return err
+	}
+	signKey, err := jwt.ParseRSAPrivateKeyFromPEM(privateKey)
+	if err != nil {
+		return err
+	}
+
+	t := jwt.NewWithClaims(
+		jwt.SigningMethodRS256,
+		&User {
+			ID: "hoge",
+		},
+	)
+
+	ts, err := t.SignedString(signKey)
+	fmt.Println(ts)
+	if err != nil {
+		return err
+	}
+	tt, err := jwt.Parse(ts, func(t *jwt.Token) (interface{}, error) {
+		publicKey, err := ioutil.ReadFile(PublicKeyPath)
+		if err != nil {
+			return nil, err
+		}
+		return jwt.ParseRSAPublicKeyFromPEM(publicKey)
+	})
+	if err != nil {
+		return err
+	}
+	fmt.Println("parsed")
+	fmt.Printf("%+v\n", tt.Claims)
+
+	return nil
 }
 
 func main() {
-	e := claimAndParse()
-	fmt.Printf("%+v\n", e)
+	fmt.Println("same key")
+	e := claimAndParseWithSameKey()
+	if e != nil {
+		fmt.Printf("%+v\n", e)
+	}
+	fmt.Println("key pair")
+	e = claimAndParseWithKeyPair()
+	if e != nil {
+		fmt.Printf("%+v\n", e)
+	}
 }
